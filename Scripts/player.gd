3d20286aab20a44a30c574c2f0e211f5c922f4a0
bengsiswanto <bengsiswantoh@gmail.com>
2023-11-30 00:46:24 +0700

    add state to player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,7 +7,24 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 
+enum {
+	MOVE,
+	ATTACK
+}
+
+var state = MOVE
+
+func _ready():
+	animation_tree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	input_vector = input_vector.normalized()
 
@@ -21,3 +38,6 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 
 	move_and_slide()
+
+func attack_state(delta):
+	pass
