ec880377af4763b2dad663f1347fd260818c761b
bengsiswanto <bengsiswantoh@gmail.com>
2023-11-30 23:30:51 +0700

    add hitbox

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
-func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		var death_effect_scene = load("res://Scenes/Effects/death_effect.tscn")
-		var death_effect = death_effect_scene.instantiate()
-		
-		death_effect.global_position = global_position
-		
-		var world = get_tree().current_scene
-		
-		world.add_child(death_effect)
-		
-		queue_free()
+func create_death_effect():
+	var death_effect_scene = load("res://Scenes/Effects/death_effect.tscn")
+	var death_effect = death_effect_scene.instantiate()
+	
+	death_effect.global_position = global_position
+	
+	var world = get_tree().current_scene
+	
+	world.add_child(death_effect)
+
+
+func _on_hurt_box_area_entered(area):
+	create_death_effect()
+	queue_free()
